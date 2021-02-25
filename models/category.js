dc0ad83b0e713633b1d22b5d342eb5667081f504
EmanuelCruz <emanuelcruz091@gmail.com>
2021-02-25 15:21:43 -0300

    model category

---
+++ models/category.js
@@ -0,0 +1,16 @@
+const { type } = require("os");
+
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define("category", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+        },
+    });
+
+    return Category;
+};
