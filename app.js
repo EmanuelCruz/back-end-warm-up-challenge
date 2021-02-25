7becfa3422039ed03349cec1c4159fd959adf937
EmanuelCruz <emanuelcruz091@gmail.com>
2021-02-25 20:32:18 -0300

    synchronize the models with the base

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
-
 const db = require("./models");
-db.sequelize.sync();
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});
+
+const controller = require("./controllers/index.js");
 
 app.get("/", function (req, res) {
     res.send("Hello World");
