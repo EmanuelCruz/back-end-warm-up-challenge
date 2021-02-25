5f0934b9f401391ccee2152faa92e34df98e8d81
EmanuelCruz <emanuelcruz091@gmail.com>
2021-02-25 15:21:21 -0300

    model post

---
+++ models/post.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const Post = sequelize.define("post", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        content: {
+            type: DataTypes.TEXT,
+        },
+        image: {
+            type: DataTypes.STRING,
+        },
+        creationDate: {
+            type: DataTypes.DATE,
+        },
+    });
+
+    return Post;
+};
