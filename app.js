b8ee3e5f02769df535aa8461bf6f9ce2f3d13832
EmanuelCruz <emanuelcruz091@gmail.com>
2021-02-25 21:18:09 -0300

    creating basic categories

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
 const express = require("express");
 const app = express();
+const apiRouter = require("./routes/api");
+
 const db = require("./models");
 
 db.sequelize.sync({ force: true }).then(() => {
     console.log("Drop and re-sync db.");
+    run();
 });
 
-const controller = require("./controllers/index.js");
+const controller = require("./controllers/index");
+const run = async () => {
+    const category1 = await controller.createCategory({
+        name: "Informática",
+    });
 
-app.get("/", function (req, res) {
-    res.send("Hello World");
-});
+    const category2 = await controller.createCategory({
+        name: "Cocina",
+    });
+
+    const category3 = await controller.createCategory({
+        name: "Deportes",
+    });
+
+    const category4 = await controller.createCategory({
+        name: "Cine",
+    });
+
+    // const post1 = await controller.createPost(1, {
+    //     title: "Title post 1",
+    //     content: "Content post1",
+    //     image: "imagenpost1.jpg",
+    //     creationDate: "2020-10-01",
+    // });
+
+    // const post2 = await controller.createPost(category1.id, {
+    //     title: "Title post 2",
+    //     content: "Content post2",
+    //     image: "imagenpost2.jpg",
+    //     creationDate: "2020-10-01",
+    // });
+
+    console.log("Datos ingresados");
+};
 
 app.listen(3000, () => {
     console.log("servidor arrancado");
